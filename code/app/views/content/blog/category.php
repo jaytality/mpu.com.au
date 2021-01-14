416b26cf88c09fc1044d85f7e42e45d3b9953cac
jaytwitch <johnathan.tiong@gmail.com>
2021-01-14 13:56:41 +1100

    updated sections listing

--- code/app/views/content/blog/category.php
+++ code/app/views/content/blog/category.php
@@ -12,17 +12,25 @@
                     if (!empty($post->featured_media)) {
                         $image = \spark\Models\BlogModel::getFeaturedMediaUrl($post->featured_media);
                         ?>
-                        <a href="/blog/post/<?=$post->slug?>">
+                        <a href="/blog/post/<?=$post->id . '-' . $post->slug?>">
                             <img src="<?=$image->source_url?>" class="card-img-top" alt="<?=$post->title->rendered?>">
                         </a>
                         <?php
                     }
+
+                    $author = \spark\Models\BlogModel::getAuthorById($post->author);
                     ?>
                     <div class="card-body">
                         <h4 class="card-title">
-                            <a href="/blog/post/<?=$post->slug?>"><?=$post->title->rendered?></a>
+                        <a href="/blog/post/<?=$post->id . '-' . $post->slug?>"><?=$post->title->rendered?></a>
                         </h4>
-                        <h6 class="card-subtitle mb-2 text-muted"><small><?=date('d/M/Y @ h:i:s', strtotime($post->date))?></small></h6>
+
+                        <h6 class="card-subtitle mb-2 text-muted">
+                            <small>
+                                <?=date('d/M/Y @ h:i:s', strtotime($post->date))?> - <?=$author->name?>
+                            </small>
+                        </h6>
+
                         <p class="card-text">
                             <?=$post->excerpt->rendered?>
                         </p>

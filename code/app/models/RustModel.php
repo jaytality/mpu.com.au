331400e41ebdfed60a1d9b5c885fb631a09d3777
jaytwitch <johnathan.tiong@gmail.com>
2021-01-22 21:07:30 +1100

    no id allocated to DB entering, so RedBean freaks out with bean finder system

--- code/app/models/RustModel.php
+++ code/app/models/RustModel.php
@@ -10,7 +10,7 @@ class RustModel extends Model
 {
     public static function getSoloPlayerStats()
     {
-        $soloplayers = R::find('rust_solos', ' WHERE name IS NOT NULL');
+        $soloplayers = R::getAll('SELECT * FROM rust_solos WHERE name IS NOT NULL');
         return $soloplayers;
     }
 }

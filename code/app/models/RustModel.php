b2684e9761a0c818749d88a6c65edccc8f57ad73
jaytwitch <johnathan.tiong@gmail.com>
2021-01-22 19:43:44 +1100

    Rust model for player data across servers

---
+++ code/app/models/RustModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace spark\Models;
+
+use \spark\Core\Model as Model;
+
+use \R as R;
+
+class RustModel extends Model
+{
+    public static function getSoloPlayerStats()
+    {
+        $soloplayers = R::find('rust_solos', ' WHERE name IS NOT NULL');
+        return $soloplayers;
+    }
+}
+
+// end of file
